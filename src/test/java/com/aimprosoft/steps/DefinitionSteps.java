29a73b929336d29b229aed6f9d4a8c2f149346e4
user-qa <guettad@mail.ru>
2017-09-20 15:35:09 +0300

    test

---
+++ src/test/java/com/aimprosoft/steps/DefinitionSteps.java
@@ -0,0 +1,37 @@
+package com.aimprosoft.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import net.thucydides.core.annotations.Steps;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import com.aimprosoft.steps.serenity.EndUserSteps;
+
+public class DefinitionSteps {
+
+    @Steps
+    EndUserSteps endUserSteps;
+
+//======================================================SignInPage======================================================
+
+    @Given("^open signin page$")
+    public void openSigninPage() throws Throwable {
+        endUserSteps.openSignInPage();
+    }
+
+    @And("^filled fields valid data$")
+    public void filledFieldsValidData() throws Throwable {
+        endUserSteps.loginAsadmin();
+    }
+
+    @When("^click on Login button$")
+    public void clickOnLoginButton() throws Throwable {
+        endUserSteps.clickOnLoginButton();
+    }
+
+    @Then("^home page is opened$")
+    public void homePageIsOpened() throws Throwable {
+
+    }
+}

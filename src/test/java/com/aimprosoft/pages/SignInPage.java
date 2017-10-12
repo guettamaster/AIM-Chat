5b4b43232b219a5d9c87657017ecebc862f6b189
user-qa <guettad@mail.ru>
2017-10-12 14:48:52 +0300

    test on the predprod env.

--- src/test/java/com/aimprosoft/pages/SignInPage.java
+++ src/test/java/com/aimprosoft/pages/SignInPage.java
@@ -8,14 +8,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.concurrent.TimeUnit;
 
-@DefaultUrl("https://chat-stage.aimprosoft.com/index.html")
+@DefaultUrl("https://chat-predprod.aimprosoft.com/index.html")
+//https://chat-stage.aimprosoft.com/index.html
 
 public class SignInPage extends net.serenitybdd.core.pages.PageObject {
 
     private static Logger logger = LoggerFactory.getLogger(SignInPage.class);
 
     public void loginAsadmin() {
-        String email = "admin@email.com";
+        String email = "admin@aimrosoft.com";
+//      admin@email.com
         String password = "qwerty1";
         logger.info("Entering email: " + email + " and password: " + password);
         $(LOCATORS.EMAIL_FIELD).sendKeys(email);

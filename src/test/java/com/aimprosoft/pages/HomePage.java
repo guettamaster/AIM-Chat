21201c2b8104ee7f0febefa5950b9aa4bda2297e
user-qa <guettad@mail.ru>
2018-04-05 13:47:50 +0300

    Scenario: 2.027 Check that message from pinned is downloaded (isn't completed)

--- src/test/java/com/aimprosoft/pages/HomePage.java
+++ src/test/java/com/aimprosoft/pages/HomePage.java
@@ -897,7 +897,7 @@ public class HomePage extends net.serenitybdd.core.pages.PageObject {
 
     public void scrollDownToMessage(String arg0) {
         withTimeoutOf(20, TimeUnit.SECONDS).waitFor(ExpectedConditions.visibilityOfElementLocated(By.xpath(LOCATORS.THE_SENT_MESSAGE_INSIDE_THE_ROOM.replace("$1", arg0))));
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", findBy(LOCATORS.THE_SENT_MESSAGE_INSIDE_THE_ROOM.replace("$1", arg0)));
+//        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", findBy(LOCATORS.THE_SENT_MESSAGE_INSIDE_THE_ROOM.replace("$1", arg0)));
         Actions actions = new Actions(getDriver());
         actions.moveToElement(findBy(LOCATORS.THE_SENT_MESSAGE_INSIDE_THE_ROOM.replace("$1", arg0))).build().perform();
     }

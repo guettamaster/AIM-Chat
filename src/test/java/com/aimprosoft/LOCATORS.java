01beb1fbef614da10d6db38201f2bd87106b1595
ivankovskiy-pc <guettamaster@gmail.com>
2019-05-20 18:00:12 +0300

    Scenario: 2.008 Check that `edited note` message to a.shevchenko is deleted (isn't completed)

--- src/test/java/com/aimprosoft/LOCATORS.java
+++ src/test/java/com/aimprosoft/LOCATORS.java
@@ -49,7 +49,7 @@ String USER_NAME_IN_THE_LEFT_PANEL = "//a[@class='room-link']//span[contains(tex
 String USER_NAME_IN_THE_HEADER = "//div[@class='channel-name room-direct'][contains(text(),'$1')]";
 String INPUT_MESSAGE_FIELD = "//div[contains(@class,'chat-container active')]//div[@class='notranslate public-DraftEditor-content']//span[@data-text='true'][contains(text(),'$1')]";
 //        "//div[@class='input-text input-text-hover']//span[@data-text='true'][contains(text(),'$1')]";
-String THE_SENT_MESSAGE_INSIDE_THE_ROOM = "//div[@class='message-main-container']//span[@class='LinkifyPlus'][contains(text(),'$1')]";
+String THE_SENT_MESSAGE_INSIDE_THE_ROOM = "(//div[@class='message-main-container']//span[@class='LinkifyPlus'][contains(text(),'$1')])[3]";
 String THE_SENT_MESSAGE1_INSIDE_THE_ROOM = "(//div[@class='message-main-container']//span[@class='LinkifyPlus'][contains(text(),'$1')])[3]";
 String RIGHT_CONTROL_HAMBURGER_ON_THE_MESSAGE = "(//span[@class='LinkifyPlus'][contains(text(), '$1')])[3]//ancestor::div[@class='message-main-container']//div[@class='right-menu-btn']";
 String MESSAGE_MENU = "//div[@class='right-control-menu']/ul";

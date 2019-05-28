4ad423bdf3e4ade4cf9dfc3b3f5e68ee2419d5fc
ivankovskiy-pc <guettamaster@gmail.com>
2019-05-28 13:37:43 +0300

    Scenario: 2.015 Check that fields in the user profile has changed (isn't completed)

--- src/test/java/com/aimprosoft/pages/HomePage.java
+++ src/test/java/com/aimprosoft/pages/HomePage.java
@@ -555,7 +555,8 @@ public class HomePage extends net.serenitybdd.core.pages.PageObject {
     }
 
     public void clickOnUserName(String arg0) {
-        withTimeoutOf(10, TimeUnit.SECONDS).waitFor(ExpectedConditions.visibilityOfElementLocated(By.xpath(LOCATORS.THE_CURRENT_AUTHORIZED_USER_NAME_IN_THE_LEFT_PANEL.replace("$1", arg0))));
+        waitABit(1500);
+//        withTimeoutOf(10, TimeUnit.SECONDS).waitFor(ExpectedConditions.visibilityOfElementLocated(By.xpath(LOCATORS.THE_CURRENT_AUTHORIZED_USER_NAME_IN_THE_LEFT_PANEL.replace("$1", arg0))));
         evaluateJavascript("arguments[0].click();", $(LOCATORS.THE_CURRENT_AUTHORIZED_USER_NAME_IN_THE_LEFT_PANEL.replace("$1", arg0)));
     }
 
